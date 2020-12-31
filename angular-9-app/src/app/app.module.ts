ad3bd37b55550560e17f3d2bf3998ce68442160d
x-tion <x_tion87@hotmail.com>
2020-12-31 16:24:54 +0700

    no message

--- angular-9-app/src/app/app.module.ts
+++ angular-9-app/src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {XxxAlertModule, XxxErrorHandlerModule, XxxLogModule, XxxMessageModule} from '@app/xxx-common';
+import {XxxHeaderModule} from '@app/modules/xxx-header/xxx-header.module';
+import {XxxHomePageModule} from '@app/modules/xxx-home-page/xxx-home-page.module';
 
 @NgModule({
   bootstrap: [AppComponent],
@@ -13,12 +17,16 @@ import { AppComponent } from './app.component';
     BrowserModule,
     MatButtonModule,
     MatIconModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    XxxAlertModule,
+    XxxErrorHandlerModule,
+    XxxHeaderModule,
+    XxxHomePageModule,
+    XxxLogModule,
+    XxxMessageModule,
+    AppRoutingModule
   ]
 })
 
 export class AppModule {
-
 }
-
-
